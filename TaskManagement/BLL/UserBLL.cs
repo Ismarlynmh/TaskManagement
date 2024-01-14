fbdfe8da88d83c4fa55d3e989f9ca49e17a91283
Ismarlyn Mendoza Hernández <Marlynm123@hotmail.com>
2024-01-14 02:21:23 -0400

    Agregar archivos de proyecto.

---
+++ TaskManagement/BLL/UserBLL.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using TaskManagement.Data;
+
+namespace TaskManagement.BLL
+{
+    public class UserBLL
+    {
+        private readonly ApplicationDbContext _contexto;
+
+        public UserBLL(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+
+        public string GetUserId(ClaimsPrincipal principal)
+        {
+            var user = _contexto.Users.FirstOrDefault(user => user.Id == principal.Identity.Name);
+            return user.Id;
+        }
+
+
+    }
+}
---
+++ TaskManagement/Models/AspNetUsers.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models
+{
+    public class AspNetUsers
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
